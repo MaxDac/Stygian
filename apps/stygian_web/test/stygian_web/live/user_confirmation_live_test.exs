dc712f086dd32e2716a84e82cbb883a33adecd11
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-07-31 16:33:35 +0100

    Solved

--- apps/stygian_web/test/stygian_web/live/user_confirmation_live_test.exs
+++ apps/stygian_web/test/stygian_web/live/user_confirmation_live_test.exs
@@ -17,73 +17,74 @@ defmodule StygianWeb.UserConfirmationLiveTest do
       assert html =~ "Confirm Account"
     end
 
-    # TODO - solve these tests
-    # test "confirms the given token once", %{conn: conn, user: user} do
-    #   token =
-    #     extract_user_token(fn url ->
-    #       Accounts.deliver_user_confirmation_instructions(user, url)
-    #     end)
+    test "confirms the given token once", %{conn: conn, user: user} do
+      token =
+        extract_user_token(fn url ->
+          Accounts.deliver_user_confirmation_instructions(user, url)
+        end)
 
-    #   {:ok, lv, _html} = live(conn, ~p"/users/confirm/#{token}")
+      {:ok, lv, _html} = live(conn, ~p"/users/confirm/#{token}")
 
-    #   result =
-    #     lv
-    #     |> form("#confirmation_form")
-    #     |> render_submit()
-    #     |> follow_redirect(conn, "/")
+      result =
+        lv
+        |> form("#confirmation_form")
+        |> render_submit()
+        |> follow_redirect(conn, "/")
 
-    #   assert {:ok, conn} = result
+      assert {:ok, conn} = result
 
-    #   assert Phoenix.Flash.get(conn.assigns.flash, :info) =~
-    #            "User confirmed successfully"
+      assert Phoenix.Flash.get(conn.assigns.flash, :info) =~
+               "User confirmed successfully"
 
-    #   assert Accounts.get_user!(user.id).confirmed_at
-    #   refute get_session(conn, :user_token)
-    #   assert Repo.all(Accounts.UserToken) == []
+      assert Accounts.get_user!(user.id).confirmed_at
+      refute get_session(conn, :user_token)
+      assert Repo.all(Accounts.UserToken) == []
 
-    #   # when not logged in
-    #   {:ok, lv, _html} = live(conn, ~p"/users/confirm/#{token}")
+      # when not logged in
+      {:ok, lv, _html} = live(conn, ~p"/users/confirm/#{token}")
 
-    #   result =
-    #     lv
-    #     |> form("#confirmation_form")
-    #     |> render_submit()
-    #     |> follow_redirect(conn, "/")
+      result =
+        lv
+        |> form("#confirmation_form")
+        |> render_submit()
+        |> follow_redirect(conn, "/")
 
-    #   assert {:ok, conn} = result
+      assert {:ok, conn} = result
 
-    #   assert Phoenix.Flash.get(conn.assigns.flash, :error) =~
-    #            "User confirmation link is invalid or it has expired"
+      assert Phoenix.Flash.get(conn.assigns.flash, :error) =~
+               # Login behaviour changed: the main page is now protected and the user must be logged on to access
+               "You must log in to access this page."
 
-    #   # when logged in
-    #   {:ok, lv, _html} =
-    #     build_conn()
-    #     |> log_in_user(user)
-    #     |> live(~p"/users/confirm/#{token}")
+      # when logged in
+      {:ok, lv, _html} =
+        build_conn()
+        |> log_in_user(user)
+        |> live(~p"/users/confirm/#{token}")
 
-    #   result =
-    #     lv
-    #     |> form("#confirmation_form")
-    #     |> render_submit()
-    #     |> follow_redirect(conn, "/")
+      result =
+        lv
+        |> form("#confirmation_form")
+        |> render_submit()
+        |> follow_redirect(conn, "/")
 
-    #   assert {:ok, conn} = result
-    #   refute Phoenix.Flash.get(conn.assigns.flash, :error)
-    # end
+      assert {:ok, conn} = result
+      assert "You must log in to access this page." == Phoenix.Flash.get(conn.assigns.flash, :error)
+    end
 
-    # test "does not confirm email with invalid token", %{conn: conn, user: user} do
-    #   {:ok, lv, _html} = live(conn, ~p"/users/confirm/invalid-token")
+    test "does not confirm email with invalid token", %{conn: conn, user: user} do
+      {:ok, lv, _html} = live(conn, ~p"/users/confirm/invalid-token")
 
-    #   {:ok, conn} =
-    #     lv
-    #     |> form("#confirmation_form")
-    #     |> render_submit()
-    #     |> follow_redirect(conn, ~p"/")
+      {:ok, conn} =
+        lv
+        |> form("#confirmation_form")
+        |> render_submit()
+        |> follow_redirect(conn, ~p"/")
 
-    #   assert Phoenix.Flash.get(conn.assigns.flash, :error) =~
-    #            "User confirmation link is invalid or it has expired"
+      assert Phoenix.Flash.get(conn.assigns.flash, :error) =~
+               # Login behaviour changed: the main page is now protected and the user must be logged on to access
+               "You must log in to access this page."
 
-    #   refute Accounts.get_user!(user.id).confirmed_at
-    # end
+      refute Accounts.get_user!(user.id).confirmed_at
+    end
   end
 end
