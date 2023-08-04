2449d5cbc15f109f3a7255df4e9205501aa129c6
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-08-04 14:21:30 +0100

    Added asdf configuration

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -3,6 +3,13 @@
 #init db
 psql --command "CREATE USER postgres WITH SUPERUSER PASSWORD 'mysecretpassword';" 
 
+# Setting up asdf
+asdf global erlang 25.3.2.5
+asdf global elixir 1.15.4-otp-25
+asdf global nodejs 18.17.0
+
+export PATH="$PATH:/home/gitpod/.asdf/shims"
+
 # Installing Phoenix packages
 mix local.hex --force
 mix local.rebar --force
