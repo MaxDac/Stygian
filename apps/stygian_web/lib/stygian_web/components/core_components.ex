a76cf2364dd2439ba77b15950acfc426ad1f6920
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-08-14 21:56:20 +0100

    Updated the flash titles (#56)

--- apps/stygian_web/lib/stygian_web/components/core_components.ex
+++ apps/stygian_web/lib/stygian_web/components/core_components.ex
@@ -149,8 +149,8 @@ defmodule StygianWeb.CoreComponents do
 
   def flash_group(assigns) do
     ~H"""
-    <.flash kind={:info} title="Success!" flash={@flash} />
-    <.flash kind={:error} title="Error!" flash={@flash} />
+    <.flash kind={:info} title="OK" flash={@flash} />
+    <.flash kind={:error} title="Errorw" flash={@flash} />
     <.flash
       id="client-error"
       kind={:error}
