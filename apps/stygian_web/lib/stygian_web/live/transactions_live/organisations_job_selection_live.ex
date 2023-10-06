75133661e1290cdd705c9d0c88f679a9bd35aa64
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-10-06 10:44:11 +0100

    temp

--- apps/stygian_web/lib/stygian_web/live/transactions_live/organisations_job_selection_live.ex
+++ apps/stygian_web/lib/stygian_web/live/transactions_live/organisations_job_selection_live.ex
@@ -8,7 +8,20 @@ defmodule StygianWeb.TransactionsLive.OrganisationsJobSelectionLive do
   alias Stygian.Organisations
 
   @impl true
-  def mount(_, _, socket) do
-    {:ok, stream(socket, :organisations, Organisations.list_organisations())}
+  def mount(_, _, %{assigns: %{current_character: current_character}} = socket) do
+    {:ok, 
+      socket
+      |> check_character_job(current_character.id)
+      |> stream(:organisations, Organisations.list_organisations())}
+  end
+
+  defp check_character_job(socket, character_id) do
+    if Organisations.has_character_organisation?(character_id) do
+      socket
+      |> put_flash(:info, "Il tuo personaggio ha già un lavoro.")
+      |> push_patch(to: ~p"/organisations")
+    else
+      socket
+    end
   end
 end
