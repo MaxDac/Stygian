7fbb08cd2d2f9b0a02ec68ed984c2c58fb1245e5
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-10-14 00:15:52 +0100

    Restored tailwind configuration file imports

--- apps/stygian_web/assets/tailwind.config.js
+++ apps/stygian_web/assets/tailwind.config.js
@@ -2,6 +2,8 @@
 // https://tailwindcss.com/docs/configuration
 
 const plugin = require("tailwindcss/plugin")
+const fs = require("fs")
+const path = require("path")
 
 module.exports = {
   content: [
