c7e7658de2da7a7d152c3ba77bf8c10a61136e7f
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-10-14 00:14:27 +0100

    Formatted

--- apps/stygian_web/lib/stygian_web/live/chat_live/map_helpers.ex
+++ apps/stygian_web/lib/stygian_web/live/chat_live/map_helpers.ex
@@ -14,10 +14,7 @@ defmodule StygianWeb.ChatLive.MapHelpers do
   def small_screen_map_selector(assigns) do
     ~H"""
     <div :for={map <- @maps} class="text-center">
-      <.link
-        navigate={@base_link <> "/#{map.id}"}
-        class="font-berolina text-2xl text-brand"
-      >
+      <.link navigate={@base_link <> "/#{map.id}"} class="font-berolina text-2xl text-brand">
         <%= map.name %>
       </.link>
     </div>
