c9db4819655f15d5f18c3d579d2a00ef9730bea8
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-10-24 11:49:27 +0000

    Adding unstaged

---
+++ apps/stygian_web/lib/stygian_web/live/admin_live/map_chat_dashboard_log_components.ex
@@ -0,0 +1,37 @@
+defmodule StygianWeb.AdminLive.MapChatDashboardLogComponents do
+  @moduledoc """
+  This component will show the chat logs for the selected filters.
+  """
+  
+  use StygianWeb, :html
+
+  @doc """
+  Renders the chat logs.
+  """
+  attr :chats, :list, required: true
+
+  def chat_logs(assigns) do
+    ~H"""
+    <div class="mt-5">
+      <ul>
+        <li :for={{_, chat_entry} <- @chats}>
+          <.chat_log_entry chat_entry={chat_entry} />
+        </li>
+      </ul>
+    </div>
+    """
+  end
+
+  @doc """
+  Renders a single chat entry.
+  """
+  attr :chat_entry, :map, required: true
+
+  def chat_log_entry(assigns) do
+    ~H"""
+    <div class="mt-5">
+      <%= @chat_entry.text %>
+    </div>
+    """
+  end
+end
