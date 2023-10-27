553992e8c5029b440cec7fec396f4a3adfe6b16b
MaxDac <massimiliano.dacunzo@hotmail.com>
2023-10-27 22:28:23 +0100

    Solving enter problem
    - Solved enter problem in chat input
    - Reduced minimum character length for chat input to 150

--- apps/stygian_web/assets/js/hooks/chat-hooks.js
+++ apps/stygian_web/assets/js/hooks/chat-hooks.js
@@ -16,15 +16,24 @@ export function addChatHooks(Hooks) {
     mounted() {
       this.el.focus()
       this.el.onkeydown = (e) => {
-        // If the value is not greater than 200 the textarea can't dispatch the event
-        if (e.key === "Enter" && this.el.value.length >= 200) {
-          // Dispatching the event, that will bubble up to the form
-          const event = new Event("submit", { bubbles: true, cancelable: true })
-          this.el.form.dispatchEvent(event)
-          
-          // Clearing the input manually because it can happen that the form
-          // from the back end gets stuck and it doesn't remove the chat input.
-          this.el.value = ""
+        if (e.key === "Enter") {
+          console.log("The element", this.el)
+          console.log("The element value length", this.el.value.length)
+          // If the value is not greater than 150 the textarea can't dispatch the event
+          if (this.el.value.length >= 150) {
+            // Dispatching the event, that will bubble up to the form
+            const event = new Event("submit", { bubbles: true, cancelable: true })
+            this.el.form.dispatchEvent(event)
+            
+            // Clearing the input manually because it can happen that the form
+            // from the back end gets stuck and it doesn't remove the chat input.
+            this.el.value = ""
+          }
+          else {
+            // If the value is not greated than 200, the phrase cannot be sent, but 
+            // the input should not allow the new line character to be added.
+            e.preventDefault()
+          }
         }
       } 
     }
